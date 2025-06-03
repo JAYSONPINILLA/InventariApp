a97e642b0f13df7207bad105824e7a9257ebc04d
Jayson Pinilla <jayson.gabriel.pinilla.suarez@gmail.com>
2025-06-03 10:33:27 -0500

    Imprime PORT y DB_URL

--- src/main/java/com/erp/inventariapp/InventariappApplication.java
+++ src/main/java/com/erp/inventariapp/InventariappApplication.java
@@ -3,11 +3,19 @@ package com.erp.inventariapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class InventariappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventariappApplication.class, args);
 	}
+
+	@PostConstruct
+	public void logStartupVars() {
+		System.out.println("✅ PORT: " + System.getenv("PORT"));
+		System.out.println("✅ DB_URL: " + System.getenv("DB_URL"));
+	}	
 
 }
